a7f593d4f282e035d9307eead3c2fb050cfa8a40
fernandaguima <fernandaguimasilva7@gmail.com>
2025-07-28 17:35:00 -0300

    Initial commit

---
+++ src/exercicios/Emprestimo.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+import java.util.Scanner;
+
+ public class Emprestimo {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        int idade;
+
+        int salario;
+
+        System.out.println("Qual a sua idade?");
+        idade = teclado.nextInt();
+
+        System.out.println("Qual o seu salario?");
+        salario = teclado.nextInt();
+
+        System.out.println("Seu nome está limpo?");
+        String nomeLimpo = teclado.next();
+
+        if (idade >=20 && salario >=3500 && nomeLimpo.equalsIgnoreCase("sim")) {
+            System.out.println("Pode fazer empréstimo");
+        } else System.out.println("Não pode fazer empréstimo");
+
+    }
+}
---
+++ src/exercicios/NotaConceito.java
@@ -0,0 +1,9 @@
+package exercicios;
+import java.util.Scanner;
+
+public class NotaConceito {
+    public static void main(String[] args) {
+
+        
+    }
+}
