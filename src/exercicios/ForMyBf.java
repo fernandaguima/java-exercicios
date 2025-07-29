1464ff77d3d556942c28ce83ae27dff72baea023
fernandaguima <fernandaguimasilva7@gmail.com>
2025-07-29 16:21:57 -0300

    formybf
    pra terminar amanha

---
+++ src/exercicios/ForMyBf.java
@@ -0,0 +1,22 @@
+package exercicios;
+import java.util.Scanner;
+
+public class ForMyBf {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        String nome;
+        String juan;
+
+        System.out.println("Qual seu nome?");
+        nome = teclado.next();
+
+        if (nome.equals("juan")) {
+            System.out.println("te amoooooooooooo");
+            else if{
+                System.out.println("xexelento");
+            }
+        }
+    }
+}
