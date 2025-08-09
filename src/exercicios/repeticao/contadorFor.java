84117521787d8a965e80cfa08d811e463d952ee7
fernandaguima <fernandaguimasilva7@gmail.com>
2025-08-09 17:45:00 -0300

    Update contadorFor.java

--- src/exercicios/repeticao/contadorFor.java
+++ src/exercicios/repeticao/contadorFor.java
@@ -3,7 +3,10 @@ package exercicios.repeticao;
 public class contadorFor {
     public static void main(String[] args) {
 
-        for (int c = 1; c <= 100; c++) {
+        //for (int c = 1; c <= 100; c++) {
+            //System.out.println(c);
+        //}
+        for (int c = 10; c >= 1; c--) {
             System.out.println(c);
         }
     }
