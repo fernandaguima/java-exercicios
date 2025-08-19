37f533eb9ff75f5603c9954eaead51524cd03e8b
fernandaguima <fernandaguimasilva7@gmail.com>
2025-08-18 22:34:51 -0300

    Create vetores.java

---
+++ src/exercicios/vetores.java
@@ -0,0 +1,14 @@
+package exercicios;
+
+public class vetores {
+    public static void main(String[] args) {
+
+        int n[] = {3, 5, 8, 2};
+
+        for(int c=0; c<=9; c++) {
+            System.out.print(n[c]);
+        }
+
+
+    }
+}
