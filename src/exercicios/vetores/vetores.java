824e5d4969cecedf9291a40ecf21ec0dd26cbfb6
fernandaguima <fernandaguimasilva7@gmail.com>
2025-08-25 13:46:02 -0300

    vetores semana

--- src/exercicios/vetores/vetores.java
+++ src/exercicios/vetores/vetores.java
@@ -1,4 +1,4 @@
-package exercicios;
+package exercicios.vetores;
 
 public class vetores {
     public static void main(String[] args) {
---
+++ src/exercicios/vetores/vetoresSemana.java
@@ -0,0 +1,19 @@
+package exercicios.vetores;
+
+public class vetoresSemana {
+    public static void main(String[] args) {
+
+        String semana[] = new String[7];
+
+        semana[0] = "domingo";
+        semana[1] = "segunda";
+        semana[2] = "terca";
+        semana[3] = "quarta";
+        semana[4] = "quinta";
+        semana[5] = "sexta";
+        semana[6] = "sabado";
+
+        System.out.println(semana[5]);
+
+    }
+}
