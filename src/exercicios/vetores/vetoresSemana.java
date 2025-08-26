8704a5372f38cf9a7a779c1930a20e52ec290383
fernandaguima <fernandaguimasilva7@gmail.com>
2025-08-26 13:08:58 -0300

    Update vetoresSemana.java

--- src/exercicios/vetores/vetoresSemana.java
+++ src/exercicios/vetores/vetoresSemana.java
@@ -3,6 +3,19 @@ package exercicios.vetores;
 public class vetoresSemana {
     public static void main(String[] args) {
 
+//        String semana[] = new String[7];
+//
+//        semana[0] = "domingo";
+//        semana[1] = "segunda";
+//        semana[2] = "terca";
+//        semana[3] = "quarta";
+//        semana[4] = "quinta";
+//        semana[5] = "sexta";
+//        semana[6] = "sabado";
+//
+//        for (int c = 0; c <=7; c++){
+//            System.out.println("o dia " + c + " da semana é: " + semana[c]);
+//        }
         String semana[] = new String[7];
 
         semana[0] = "domingo";
@@ -13,8 +26,8 @@ public class vetoresSemana {
         semana[5] = "sexta";
         semana[6] = "sabado";
 
-        for (int c = 0; c <=7; c++){
-            System.out.println("o dia " + c + " da semana é: " + semana[c]);
+        for (String dia : semana) {
+            System.out.println("O dia da semana é " + dia);
         }
     }
 }
