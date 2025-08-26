1045bd7a6b639414bdae35ecaf54a551860bef49
fernandaguima <fernandaguimasilva7@gmail.com>
2025-08-26 15:47:44 -0300

    Update vetoresSemana.java
    deu erro quando coloquei <=6 porque ele rodava ate o -1 dava erro pq n tem -1 entao tem q ser >=0 porque ele vai parar quando chegar no 0

--- src/exercicios/vetores/vetoresSemana.java
+++ src/exercicios/vetores/vetoresSemana.java
@@ -3,19 +3,6 @@ package exercicios.vetores;
 public class vetoresSemana {
     public static void main(String[] args) {
 
-//        String semana[] = new String[7];
-//
-//        semana[0] = "domingo";
-//        semana[1] = "segunda";
-//        semana[2] = "terca";
-//        semana[3] = "quarta";
-//        semana[4] = "quinta";
-//        semana[5] = "sexta";
-//        semana[6] = "sabado";
-//
-//        for (int c = 0; c <=7; c++){
-//            System.out.println("o dia " + c + " da semana é: " + semana[c]);
-//        }
         String semana[] = new String[7];
 
         semana[0] = "domingo";
@@ -26,8 +13,33 @@ public class vetoresSemana {
         semana[5] = "sexta";
         semana[6] = "sabado";
 
-        for (String dia : semana) {
-            System.out.println("O dia da semana é " + dia);
+        //deu erro quando coloquei <=6 porque ele rodava ate o -1 dava erro pq n tem -1 entao tem q ser >=0 porque ele vai parar quando chegar no 0
+        for (int c = 6; c >=0; c--){
+            System.out.println("o dia " + c + " da semana é: " + semana[c]);
         }
+
+
+
+
+
+
+
+
+
+
+
+//        String semana[] = new String[7];
+//
+//        semana[0] = "domingo";
+//        semana[1] = "segunda";
+//        semana[2] = "terca";
+//        semana[3] = "quarta";
+//        semana[4] = "quinta";
+//        semana[5] = "sexta";
+//        semana[6] = "sabado";
+//
+//        for (String dia : semana) {
+//            System.out.println("O dia da semana é " + dia);
+//        }
     }
 }
