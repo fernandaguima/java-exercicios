79bee617b1e28513eac48a5775093ded237a2dc9
fernandaguima <fernandaguimasilva7@gmail.com>
2025-09-12 17:06:51 -0300

    construtor2

--- src/POO/Caneta2.java
+++ src/POO/Caneta2.java
@@ -4,6 +4,14 @@ public class Caneta2 {
 
     public String modelo;
     private float ponta;
+    private boolean tampada;
+    private String cor;
+
+    public Caneta2() {
+        this.tampar(); //metodo
+        this.cor = "Azul"; //atributo ATENCAO
+
+    }
 
     public String getModelo() {
         return this.modelo;
@@ -21,8 +29,18 @@ public class Caneta2 {
         this.ponta = p;
     }
 
+    public void tampar() {
+        this.tampada = true;
+    }
+
+    public void destampada() {
+        this.tampada = false;
+    }
+
     public void status() {
         System.out.println("Modelo: " + this.modelo);
         System.out.println("Ponta: " + this.ponta);
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Tampada? " + this.tampada);
     }
 }
--- src/POO/Caneta3.java
+++ src/POO/Caneta3.java
@@ -4,7 +4,7 @@ public class Caneta3 {
     public static void main(String[] args) {
 
         Caneta2 p2 = new Caneta2();
-        // se eu fizesse sem o setter ia funcionar pq eh publico 
+        // se eu fizesse sem o setter ia funcionar pq eh publico
         p2.setModelo("BIC Cristal");
         p2.setPonta(0.7f);
         p2.status();
