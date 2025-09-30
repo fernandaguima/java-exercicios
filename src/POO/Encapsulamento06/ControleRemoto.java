48c9dc081fba4561c5a1b5540f714009ffa31469
fernandaguima <fernandaguimasilva7@gmail.com>
2025-09-30 09:32:43 -0300

    Update ControleRemoto.java

--- src/POO/Encapsulamento06/ControleRemoto.java
+++ src/POO/Encapsulamento06/ControleRemoto.java
@@ -1,4 +1,46 @@
 package POO.Encapsulamento06;
 
 public class ControleRemoto {
+  // atributos
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+
+    public ControleRemoto() {
+        this.volume = 50;
+        this.ligado = false;
+        this.tocando = false;
+    }
+  // métodos
+
+
+
+
+
+
+ // getters e setters
+    private int getVolume() {
+        return this.volume;
+    }
+
+    private void setVolume(int v) {
+        this.volume = v;
+    }
+
+    private boolean getLigado() {
+        return this.ligado;
+    }
+
+    private void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    private boolean getTocando() {
+        return this.tocando;
+    }
+
+    private void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+
 }
