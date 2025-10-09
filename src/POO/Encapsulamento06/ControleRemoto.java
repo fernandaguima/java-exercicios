14ca76f1febffdd45b798868b2e1829c0ed59b53
fernandaguima <fernandaguimasilva7@gmail.com>
2025-10-09 20:12:32 -0300

    metodos controle

--- src/POO/Encapsulamento06/ControleRemoto.java
+++ src/POO/Encapsulamento06/ControleRemoto.java
@@ -13,6 +13,62 @@ public class ControleRemoto {
     }
   // métodos
 
+    public void ligar() {
+        this.setLigado(true);
+    }
+
+    public void desligar() {
+        this.setLigado(false);
+    }
+
+    public void abrirMenu() {
+        System.out.println("Está ligado? " + this.getLigado());
+        System.out.println("Está tocando? " + this.getTocando());
+        System.out.println("Volume: " + this.getVolume());
+        for (int i = 0; i <= this.getVolume(); i += 10) {
+            System.out.println("|");
+        }
+    }
+
+    public void fecharMenu() {
+        System.out.println("Fechando o menu...");
+    }
+
+    public void maisVolume() {
+        if (this.getLigado()); {
+            this.setVolume(getVolume() + 5);
+        }
+    }
+
+    public void menosVolume() {
+        if (this.getLigado()); {
+            this.setVolume(this.getVolume() - 5);
+        }
+    }
+
+    public void ligarMudo() {
+        if (this.getLigado() && this.getVolume() > 0) {
+            this.setVolume(0);
+        }
+    }
+
+    public void desligarMudo() {
+        if (this.getLigado() && this.getVolume() == 0); {
+            this.setVolume(50);
+        }
+    }
+
+    public void play() {
+        if (this.getLigado() && !(this.getTocando())); {
+            this.setTocando(true);
+        }
+    }
+
+    public void pause() {
+        if (this.getLigado() && this.getTocando()); {
+            this.setTocando(false);
+        }
+    }
 
 
 
@@ -42,5 +98,4 @@ public class ControleRemoto {
     private void setTocando(boolean tocando) {
         this.tocando = tocando;
     }
-
 }
