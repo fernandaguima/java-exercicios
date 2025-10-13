cf2198cf0eafc52412b90dd6936d8bc6ff941316
fernandaguima <fernandaguimasilva7@gmail.com>
2025-10-13 20:18:19 -0300

    Update Apolice.java

--- src/exercicios/dolivro/exercicio01/Apolice.java
+++ src/exercicios/dolivro/exercicio01/Apolice.java
@@ -1,4 +1,18 @@
 package exercicios.dolivro.exercicio01;
 
 public class Apolice {
+
+    String nome;
+    int idade;
+    double valorPremio;
+
+    public void imprimir() {
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(valorPremio);
+    }
+
+    public void calcularPremioApolice() {
+        
+    }
 }
