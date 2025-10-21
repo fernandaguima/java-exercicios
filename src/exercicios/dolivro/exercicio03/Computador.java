409e8813fe7b5c80a380f06c534fe3af38643d24
fernandaguima <fernandaguimasilva7@gmail.com>
2025-10-21 08:29:43 -0300

    exercicios

---
+++ src/exercicios/dolivro/exercicio03/Computador.java
@@ -0,0 +1,36 @@
+package exercicios.dolivro.exercicio03;
+
+public class Computador {
+
+    String marca, cor, modelo;
+    int numeroSerie;
+    double preco;
+
+    public void imprimir() {
+        System.out.println("Marca : " + this.marca);
+        System.out.println("Modelo :" + this.modelo);
+        System.out.println("Número de série: " + this.numeroSerie);
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Preco: " + this.preco);
+    }
+
+    public void calcularValor() {
+        if (marca.equals("HP")) {
+            this.preco = this.preco * 1.30;
+        }
+        if (marca.equals("IBM")) {
+            this.preco = this.preco * 1.50;
+        }
+        else {
+            this.preco = this.preco;
+        }
+    }
+
+    public int alterarValor(double novoValor) {
+        if (novoValor > 0) {
+            this.preco = novoValor;
+            return 1;
+        }
+        return 0;
+    }
+}
---
+++ src/exercicios/dolivro/exercicio03/PirncipalComputador.java
@@ -0,0 +1,33 @@
+package exercicios.dolivro.exercicio03;
+
+public class PirncipalComputador {
+
+    public static void main(String[] args) {
+
+        Computador novo1 = new Computador();
+
+        novo1.marca = "HP";
+        novo1.cor = "Preto";
+        novo1.modelo = "L1485";
+        novo1.numeroSerie = 12345;
+        novo1.preco = 3000;
+
+        System.out.println("Primeiro Computador:");
+        novo1.calcularValor();
+        novo1.imprimir();
+
+        Computador novo2 = new Computador();
+
+        novo2.marca = "IBM";
+        novo2.cor = "Azul";
+        novo2.modelo = "F1006";
+        novo2.numeroSerie = 54321;
+        novo2.preco = 2000;
+
+        System.out.println("Segundo computador:");
+        novo2.calcularValor();
+        novo2.imprimir();
+
+
+    }
+}
