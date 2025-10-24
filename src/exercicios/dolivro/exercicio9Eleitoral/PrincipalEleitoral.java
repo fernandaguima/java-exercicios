b3cc9e5bcd5dbb36b6ea1113f388959efdd1c2d0
fernandaguima <fernandaguimasilva7@gmail.com>
2025-10-24 10:41:38 -0300

    Update PrincipalEleitoral.java

--- src/exercicios/dolivro/exercicio9Eleitoral/PrincipalEleitoral.java
+++ src/exercicios/dolivro/exercicio9Eleitoral/PrincipalEleitoral.java
@@ -1,4 +1,14 @@
 package exercicios.dolivro.exercicio9Eleitoral;
 
 public class PrincipalEleitoral {
+
+    public static void main(String[] args) {
+
+        Eleitoral novo = new Eleitoral();
+
+        novo.nome = "theo";
+        novo.idade = 20;
+
+        novo.imprimir();
+    }
 }
