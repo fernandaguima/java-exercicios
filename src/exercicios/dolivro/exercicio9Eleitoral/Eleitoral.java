6241ef57d6053f5839a6a238d6762e5e3df09632
fernandaguima <fernandaguimasilva7@gmail.com>
2025-10-24 10:39:33 -0300

    Create Eleitoral.java

---
+++ src/exercicios/dolivro/exercicio9Eleitoral/Eleitoral.java
@@ -0,0 +1,23 @@
+package exercicios.dolivro.exercicio9Eleitoral;
+
+public class Eleitoral {
+
+    String nome;
+    int idade;
+
+    public void imprimir() {
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Idade: " + this.idade);
+        verificar();
+    }
+
+    public void verificar() {
+        if (idade<16) {
+            System.out.println("Eleitor não pode votar");
+        } if ((idade >= 16) && (idade <= 65)) {
+            System.out.println("Eleitor deve votar");
+        } if (idade > 65) {
+            System.out.println("Voto facultativo");
+        }
+    }
+}
