4d3c7e86a94977ecf699be419a3479a621e3a828
fernandaguima <fernandaguimasilva7@gmail.com>
2025-11-24 08:58:51 -0300

    Update vetores.java

--- src/exercicios/vetores/vetores.java
+++ src/exercicios/vetores/vetores.java
@@ -3,18 +3,32 @@ package exercicios.vetores;
 public class vetores {
     public static void main(String[] args) {
 
-        int[] vetor = {2, 4, 2, 1};
-        int soma = 0; // variavel
+//        int[] vetor = {2, 4, 2, 1};
+//        int soma = 0; // variavel
+//
+//        for (int i = 0; i < vetor.length; i++) {
+//            soma += vetor[i];
+//
+//        } // meu erro aqui foi que eu tava colocando o sout dentro do loop
+//
+//       System.out.println("A soma é: " + soma);
 
-        for (int i = 0; i < vetor.length; i++) {
-            soma += vetor[i];
 
-        } // meu erro aqui foi que eu tava colocando o sout dentro do loop
-
-        System.out.println("A soma é: " + soma);
-//        int[] numeros = {1, 2, 3, 4, 5};
+//        int[] numeros = {1, 2, 3, 4, 9};
 //        for (int i = 0; i < numeros.length; i++) {
 //            System.out.println(numeros[i]);
 //        }
+
+//        int[] numeros = {1,2,3};
+//
+//                System.out.println(numeros[0]);
+//                System.out.println(numeros[2]);
+//
+//        int soma;
+//        int[] numeros = {1, 2, 3, 4, 5};
+//            soma = numeros[0] + numeros[3];
+//            System.out.println("A soma é: " + soma);
+
+        }
     }
-}
+
