3964f67e9658114e27d560ca81dec7c9ca6297c3
fernandaguima <fernandaguimasilva7@gmail.com>
2025-12-15 08:39:20 -0300

    Create Aula011.java

---
+++ src/exercicios/POO/heranca/guanabara/aula11/Aula011.java
@@ -0,0 +1,14 @@
+package exercicios.POO.heranca.guanabara.aula11;
+
+public class Aula011 {
+    public static void main(String[] args) {
+
+        Aluno a1 = new Aluno();
+        a1.setNome("Theo");
+        a1.setMatricula(1111);
+        a1.setCurso("Informática");
+        a1.setIdade(19);
+        a1.setSexo("Masculino");
+        a1.pagarMensalidade();
+    }
+}
