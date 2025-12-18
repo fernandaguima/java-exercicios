f91383a7a69d886d17e802ecd3d62154e9b3f831
fernandaguima <fernandaguimasilva7@gmail.com>
2025-12-18 10:03:24 -0300

    Update Mamifero.java

--- src/exercicios/POO/polimorfismo/Aula012/Mamifero.java
+++ src/exercicios/POO/polimorfismo/Aula012/Mamifero.java
@@ -1,4 +1,21 @@
 package exercicios.POO.polimorfismo.Aula012;
 
-public class Mamifero {
+public class Mamifero extends Animal {
+
+    private String corPelo;
+
+    @Override
+    public void locomover() {
+        System.out.println("Correndo");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Mamando");
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Fazendo som de mamífero");
+    }
 }
