ec8e71843afc14aac18a59cd791f50fc1f689d42
fernandaguima <fernandaguimasilva7@gmail.com>
2025-12-22 10:39:40 -0300

    Create Aula013Principal.java

---
+++ src/exercicios/POO/polimorfismo/Aula013/Aula013Principal.java
@@ -0,0 +1,10 @@
+package exercicios.POO.polimorfismo.Aula013;
+
+public class Aula013Principal {
+    public static void main(String[] args) {
+        Cachorro c = new Cachorro();
+
+        c.reagir(11, 45);
+        c.reagir(true);
+    }
+}
