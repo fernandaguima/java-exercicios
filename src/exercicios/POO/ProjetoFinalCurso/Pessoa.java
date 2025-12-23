e1a433cc00e7ff19c38a4883a3b5b3d3ffc94243
fernandaguima <fernandaguimasilva7@gmail.com>
2025-12-23 10:19:19 -0300

    Update Pessoa.java

--- src/exercicios/POO/ProjetoFinalCurso/Pessoa.java
+++ src/exercicios/POO/ProjetoFinalCurso/Pessoa.java
@@ -1,4 +1,11 @@
 package exercicios.POO.ProjetoFinalCurso;
 
-public class Pessoa {
+public abstract class Pessoa {
+
+    protected String nome;
+    protected int idade;
+    protected String sexo;
+    protected String experiencia;
+
+    protected abstract void ganharExp();
 }
