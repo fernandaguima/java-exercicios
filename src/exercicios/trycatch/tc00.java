a4b8e26f80932ee6436cfa6916ac65a6f089955e
fernandaguima <fernandaguimasilva7@gmail.com>
2026-03-03 19:46:45 -0300

    Create tc00.java

---
+++ src/exercicios/trycatch/tc00.java
@@ -0,0 +1,22 @@
+package exercicios.trycatch;
+
+import java.util.Scanner;
+
+public class tc00 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o preço:");
+        String entrada = sc.nextLine();
+
+        double preco = 0;
+
+        try {
+            preco = Double.parseDouble(entrada);;
+        } catch (NumberFormatException e) {
+            System.out.println("Preço inválido. Digite apenas números.");
+        }
+        System.out.println("Preço digitado: " + preco);
+    }
+}
