928eb7d9442eea830fdfa89c6a0763c0247f7dfd
fernandaguima <fernandaguimasilva7@gmail.com>
2026-04-27 09:10:29 -0300

    Update Produto.java

--- src/exercicios/carrinhodecompras/Produto.java
+++ src/exercicios/carrinhodecompras/Produto.java
@@ -1,4 +1,23 @@
 package exercicios.carrinhodecompras;
 
 public class Produto {
+
+    private String nome;
+    private double preco;
+    private int quant;
+
+    public Produto(String nome, double preco, int quant) {
+        if (nome == null || nome.isEmpty()) {
+            throw new IllegalArgumentException("Nome não pode ser vazio");
+        }
+        if (preco < 0) {
+            throw new IllegalArgumentException("Preço não pode ser negativo");
+        }
+        if (quant <= 0) {
+            throw new IllegalArgumentException("Insira uma quantidade válida");
+        }
+        this.nome = nome;
+        this.preco = preco;
+        this.quant = quant;
+    }
 }
